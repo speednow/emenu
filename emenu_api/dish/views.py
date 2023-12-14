b81dc0415888837420f1608bee09717a8a54bf17
Alan Bednarczyk <fubu36963421@gmail.com>
2023-12-14 20:43:55 +0100

    feat: Caching improvements

--- emenu_api/dish/views.py
+++ emenu_api/dish/views.py
@@ -1,7 +1,8 @@
 from dish.models import Dish
 from django.core.cache import caches
+from django.db import transaction
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, permissions, viewsets
+from rest_framework import filters, permissions, status, viewsets
 from rest_framework.response import Response
 
 from .filters import DishFilter
@@ -28,7 +29,7 @@ class DishViewSet(viewsets.ModelViewSet):
 
         if not cache_dishes:
             response = super().list(request, *args, **kwargs)
-            self.cache.set(cache_key, response.data, timeout=180)  # Cachuje na 3 minuty
+            self.cache.set(cache_key, response.data)
             return response
         return Response(cache_dishes)
 
@@ -37,24 +38,44 @@ class DishViewSet(viewsets.ModelViewSet):
         cache_dish = self.cache.get(cache_key)
         if not cache_dish:
             response = super().retrieve(request, *args, **kwargs)
-            self.cache.set(cache_key, response.data, timeout=180)
+            self.cache.set(cache_key, response.data)
             return response
         return Response(cache_dish)
 
-    def clear_dish_cache(self):
-        self.cache.delete_pattern("dish_list_*")
+    def clear_cache(self, dish_id):
+        dish_detail_key = f"dish_detail_{dish_id}"
+        cache_patterns = [
+            "dish_list_*",
+            "menu_list_*",
+            "menu_detail_*",
+        ]  # if you want to optimise menu_detail you can take dish_id, where dish in mensu exists nd clean only few details
+        with transaction.atomic():
+            for pattern in cache_patterns:
+                self.cache.delete_pattern(pattern)
+
+            detail_to_delete = self.cache.get(dish_detail_key)
+            if detail_to_delete:
+                self.cache.delete(dish_detail_key)
 
     def create(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
-        self.clear_dish_cache()
+        self.clear_cache(response.data.get("id"))
         return response
 
     def update(self, request, *args, **kwargs):
         response = super().update(request, *args, **kwargs)
-        self.clear_dish_cache()
+        self.clear_cache(response.data.get("id"))
         return response
 
     def destroy(self, request, *args, **kwargs):
-        response = super().destroy(request, *args, **kwargs)
-        self.clear_dish_cache()
+        instance = self.get_object()
+        instance_id = instance.id
+
+        try:
+            response = super().destroy(request, *args, **kwargs)
+        except Exception:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        self.clear_cache(instance_id)
+
         return response
--- emenu_api/menu/views.py
+++ emenu_api/menu/views.py
@@ -1,8 +1,9 @@
 from dish.models import Dish
 from django.core.cache import caches
+from django.db import transaction
 from django.db.models import Count, Prefetch
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, permissions, viewsets
+from rest_framework import filters, permissions, status, viewsets
 from rest_framework.response import Response
 
 from .filters import MenuFilter
@@ -29,8 +30,19 @@ class MenuViewSet(viewsets.ModelViewSet):
             queryset = queryset.annotate(dishes_count=Count("dishes")).filter(dishes_count__gt=0)
         return queryset.order_by("-updated_at")
 
-    def clear_menu_cache(self):
-        self.cache.delete_pattern("menu_list_*")
+    def clear_cache(self, menu_id):
+        menu_detail_key_auth = f"menu_detail_auth_{menu_id}"
+        menu_detail_key_non_auth = f"menu_detail_anon_{menu_id}"
+        with transaction.atomic():
+            self.cache.delete_pattern("menu_list_*")
+
+            detail_to_delete = self.cache.get(menu_detail_key_auth)
+            if detail_to_delete:
+                self.cache.delete(menu_detail_key_auth)
+
+            detail_to_delete = self.cache.get(menu_detail_key_non_auth)
+            if detail_to_delete:
+                self.cache.delete(menu_detail_key_non_auth)
 
     def get_cache_key(self, request, *args, **kwargs):
         is_authenticated = "auth" if request.user.is_authenticated else "anon"
@@ -42,30 +54,39 @@ class MenuViewSet(viewsets.ModelViewSet):
 
         if not cache_menus:
             response = super().list(request, *args, **kwargs)
-            self.cache.set(cache_key, response.data, timeout=180)
+            self.cache.set(cache_key, response.data)
             return response
         return Response(cache_menus)
 
     def retrieve(self, request, *args, **kwargs):
-        cache_key = f'menu_detail_{kwargs["pk"]}'
+        is_authenticated = "auth" if request.user.is_authenticated else "anon"
+        cache_key = f'menu_detail_{is_authenticated}_{kwargs["pk"]}'
         cache_menu = self.cache.get(cache_key)
         if not cache_menu:
             response = super().retrieve(request, *args, **kwargs)
-            self.cache.set(cache_key, response.data, timeout=180)
+            self.cache.set(cache_key, response.data)
             return response
         return Response(cache_menu)
 
     def create(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
-        self.clear_menu_cache()
+        self.clear_cache(response.data.get("id"))
         return response
 
     def update(self, request, *args, **kwargs):
         response = super().update(request, *args, **kwargs)
-        self.clear_menu_cache()
+        self.clear_cache(response.data.get("id"))
         return response
 
     def destroy(self, request, *args, **kwargs):
-        response = super().destroy(request, *args, **kwargs)
-        self.clear_menu_cache()
+        instance = self.get_object()
+        instance_id = instance.id
+
+        try:
+            response = super().destroy(request, *args, **kwargs)
+        except Exception:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        self.clear_cache(instance_id)
+
         return response
